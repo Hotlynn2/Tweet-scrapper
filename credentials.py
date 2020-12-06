85aa43a140e431e9a3f2c268a97571ebf071c474
Franklin Adjei <laizer421@gmail.com>
2020-12-06 23:28:28 +0000

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,6 @@
+# this is for authorization
+
+consumer_key = 'pl13zULs9D5CZvvywVw9Qe9BU'
+consumer_secret ='bWcUYdpqsjDOrgAmUV42nK81uZDi0bZ4C9RDHxDcwyUAAI8KaN'
+access_token = '814433953641824256-JNNefTPysP2UVNpu4aY6Y6qDbnNydLr'
+access_token_secret = '9t1WaQbco5tE8gMMLCPTl2u0AwxVNHCCmVaPZrDePywc0'
